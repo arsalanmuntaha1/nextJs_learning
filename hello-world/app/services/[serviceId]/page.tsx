2563f8d3ab1d3221514541362d7652e7a0906295
arsalanmuntaha1 <arsalanmuntaha1@gmail.com>
2024-10-10 02:59:21 +0500

    assignment done

--- hello-world/app/services/[serviceId]/page.tsx
+++ hello-world/app/services/[serviceId]/page.tsx
@@ -8,7 +8,10 @@ export default function serviceId() {
                 <p>
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe possimus vel
                     porro est debitis mollitia sunt ad? Vel, beatae aliquid! Exercitationem
-                    recusandae modi facere odit iste voluptate reiciendis ipsam fugit!
+                    recusandae modi facere odit iste voluptate reiciendis ipsam fugit! Lorem ipsum
+                    dolor, sit amet consectetur adipisicing elit. Facere beatae dicta debitis
+                    repellendus eaque quisquam, eos explicabo facilis, nostrum deserunt quas est
+                    vero distinctio, iure perspiciatis optio. Laboriosam, a dolorum!
                 </p>
             </div>
         </div>
